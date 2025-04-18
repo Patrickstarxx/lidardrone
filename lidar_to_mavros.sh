24f35509d797ff5a1845a525c6b40d8e53a575f4
xxx <Patrickstarxx@163.com>
2025-04-18 20:40:44 +0800

    一件启动

---
+++ lidar_to_mavros.sh
@@ -0,0 +1,9 @@
+source ./devel/setup.bash
+roslaunch mavros px4.launch & sleep 10;
+roslaunch lidar_to_mavros lidar_to_mavros.launch & sleep 5;
+rosrun mavros mavcmd long 511 32 8000 0 0 0 0 0;
+roslaunch ego_planner single_run_in_exp.launch & sleep 5;
+roslaunch controller wypts_ctrl.launch & sleep 5;
+rosrun controller cxr_egoctrl_v1 & sleep 5;
+rosrun cam aruco_img.py;
+wait;
